64e310439ab626ca3c8a96d13ef48b1b6e920088
aeren <tiltingwindmill1564@gmail.com>
2024-08-26 12:04:25 +0900

    Update mersenne_twister_breaker.py

--- Library/mersenne_twister_breaker.py
+++ Library/mersenne_twister_breaker.py
@@ -9,18 +9,67 @@ class mersenne_twister_breaker:
 	T, C = 15, 0xEFC60000
 	L = 18
 	F = 1812433253
-	@staticmethod
-	def _sanitize(x, w):
-		assert isinstance(x, (int, str))
+	def _sanitize(self, x, w):
+		assert isinstance(x, (int, bytes, str))
 		if isinstance(x, int):
 			assert 0 <= x < 2**w
 			x = "".join([str(x >> i & 1) for i in range(w)])
+		elif isinstance(x, bytes):
+			assert len(x) == w
+			w *= 8
+			x = "".join([str(x[i >> 3] >> i % 8 & 1) for i in range(w)])
 		assert len(x) == w
 		assert all(c in "01?" for c in x)
 		return x
-	# https://stackered.com/blog/python-random-prediction/
-	@staticmethod
-	def recover_seed_array_from_state(state, subtract_indices):
+	# From https://stackered.com/blog/python-random-prediction
+	def _unshift_right(self, x, shift):
+		res = x
+		for i in range(32):
+			res = x ^ res >> shift
+		return res
+	# From https://stackered.com/blog/python-random-prediction
+	def _unshift_Left(self, x, shift, mask):
+		res = x
+		for i in range(32):
+			res = x ^ (res << shift & mask)
+		return res
+	# From https://stackered.com/blog/python-random-prediction
+	def untemper(self, v):
+		v = self._unshift_right(v, 18)
+		v = self._unshift_Left(v, 15, 0xefc60000)
+		v = self._unshift_Left(v, 7, 0x9d2c5680)
+		v = self._unshift_right(v, 11)
+		return v
+	# From https://stackered.com/blog/python-random-prediction
+	def _invert_step(self, si, si227):
+		X = si ^ si227
+		mti1 = (X & 0x80000000) >> 31
+		if mti1:
+			X ^= self.A
+		mti = X << 1 & 0x80000000
+		mti1 += X << 1 & 0x7FFFFFFF
+		return mti, mti1
+	# From https://stackered.com/blog/python-random-prediction
+	# Note that the first state value is never used except for its MSB
+	def rewind_state(self, state):
+		prev_state = [0] * 624
+		s = state[ : ]
+		I, I0 = self._invert_step(s[self.N - 1], s[self.M - 1])
+		prev_state[self.N - 1] = I
+		# this does nothing when working with a known full state, but is important we rewinding more than 1 time
+		s[0] = (s[0] & 0x80000000) + I0
+		for i in reversed(range(self.N - self.M, self.N - 1)):
+			I, I1 = self._invert_step(s[i], s[i - (self.N - self.M)])
+			prev_state[i] += I
+			prev_state[i + 1] += I1
+		for i in reversed(range(self.N - self.M)):
+			I, I1 = self._invert_step(s[i], prev_state[i + self.M])
+			prev_state[i] += I
+			prev_state[i + 1] += I1
+		# The LSBs of prev_state[0] do not matter, they are 0 here
+		return prev_state[ : ]
+	# From https://stackered.com/blog/python-random-prediction
+	def recover_seed_array_from_state(self, state, subtract_indices):
 		def _init_genrand(seed):
 			MT = [0] * 624
 			MT[0] = seed & 0xFFFFFFFF
@@ -46,7 +95,7 @@ class mersenne_twister_breaker:
 		seed[623] = s[1] - (s1_old ^ (s[0] ^ s[0] >> 30) * 1664525) & 0xFFFFFFFF
 		if subtract_indices:
 			seed = [(2**32 + e - i) % 2**32 for i, e in enumerate(seed)]
-		return seed
+		return seed[ : ]
 	# Goal is to recover the initial state
 	def init_state(self, init_index):
 		assert 1 <= init_index <= self.N
@@ -78,26 +127,35 @@ class mersenne_twister_breaker:
 			if equation >> i & 1:
 				eqs ^= self.twister.state[i]
 		assert self.solver.add_equation_if_consistent(eqs, output)
-	# if x is a string, it must of length self.W consisting of characters in "01?", representing values in little endian order
+	# if x is a string, it must of length self.W consisting of characters in "01?"
 	def setrand_uint(self, x):
+		assert isinstance(x, (int, str))
 		assert self.recovery_mode in range(3)
 		x = self._sanitize(x, self.W)
 		eqs = self.twister.genrand_uint()
 		for i, v in enumerate(reversed(eqs)):
 			if x[i] != '?':
 				assert self.solver.add_equation_if_consistent(eq, int(x[i]))
-	# if x is a string, it must of length n consisting of characters in "01?", representing values in little endian order
+	# if x is an integer, it is the same as python random.getrandbits
+	# if x is a string, it must of length n consisting of characters in "01?"
 	def setrandbits(self, n, x):
+		assert isinstance(x, (int, str))
 		assert self.recovery_mode in range(3)
 		x = self._sanitize(x, n)
 		eqs = self.twister.getrandbits(n)
 		for i, eq in enumerate(reversed(eqs)):
 			if x[i] != '?':
 				assert self.solver.add_equation_if_consistent(eq, int(x[i]))
-	# if x is a string, it must of length 8 * n consisting of characters in "01?", representing values in little endian order
+	# if x is a bytes, it is the same as python random.randbytes
+	# if x is a string, it must of length 8 * n consisting of characters in "01?"
 	def setrandbytes(self, n, x):
+		assert isinstance(x, (bytes, str))
 		assert self.recovery_mode in range(3)
-		self.setrandbits(8 * n, x)
+		x = self._sanitize(x, n)
+		eqs = self.twister.getrandbits(8 * n)
+		for i, eq in enumerate(reversed(eqs)):
+			if x[i] != '?':
+				assert self.solver.add_equation_if_consistent(eq, int(x[i]))
 	def recover(self):
 		assert self.recovery_mode in range(3)
 		assignment, basis = self.solver.solve()
@@ -116,17 +174,35 @@ class mersenne_twister_breaker:
 			for l in range(len(pref) - 12):
 				if sha512(pref[-l : ]).digest()[ : -8] == h[ : -8]:
 					return pref[-l : ]
+			print("<ERROR - mersenne_twister_breaker> Failed to recover the seed")
 			assert False
 
 if __name__ == "__main__":
 	import random
+
+	def test_rewind():
+		r = random.Random(12345)
+		I = list(random.getstate()[1][:-1])
+		breaker = mersenne_twister_breaker()
+		S1 = [breaker.untemper(random.getrandbits(32)) for _ in range(624)]
+		S2 = [breaker.untemper(random.getrandbits(32)) for _ in range(624)]
+		S3 = [breaker.untemper(random.getrandbits(32)) for _ in range(624)]
+		I_ = breaker.rewind_state(S1)
+		S2_ = breaker.rewind_state(S3)
+		S1_ = breaker.rewind_state(S2)
+		assert I_[1 : ] == I[1 : ]
+		assert S1_[1 : ] == S1[1 : ]
+		assert S2_[1 : ] == S2[1 : ]
+		I_ = breaker.rewind_state(breaker.rewind_state(breaker.rewind_state(S3)))
+		assert I_ == I
+		print(f"[test_recover_seed_from_state] finished")
 
 	def test_recover_seed_from_state():
 		r = random.Random()
 		seed = 923534439
 		r.seed(seed)
 		breaker = mersenne_twister_breaker()
-		recovered = breaker.recover_seed_array_from_state(r.getstate()[1])[2]
+		recovered = breaker.recover_seed_array_from_state(r.getstate()[1], False)[2]
 		assert recovered == seed
 		print(f"[test_recover_seed_from_state] finished")
 
@@ -136,9 +212,9 @@ if __name__ == "__main__":
 		r.seed(seed)
 		breaker = mersenne_twister_breaker()
 		breaker.init_seed()
-		for i in range(624):
+		for i in range(312):
 			print(f"[test_recover_seed] {i = }")
-			breaker.setrandbits(32, r.getrandbits(32))
+			breaker.setrandbits(64, r.getrandbits(64))
 		assert breaker.recover() == seed
 		print(f"[test_recover_seed] Finished")
 
@@ -148,13 +224,14 @@ if __name__ == "__main__":
 		r.seed(byteseed)
 		breaker = mersenne_twister_breaker()
 		breaker.init_byteseed()
-		for i in range(624):
+		for i in range(156):
 			print(f"[test_recover_byteseed] {i = }")
-			breaker.setrandbits(32, r.getrandbits(32))
+			breaker.setrandbytes(32, r.randbytes(32))
 		recovered = breaker.recover()
 		assert recovered == byteseed
 		print(f"[test_recover_byteseed] Finished")
 
+	test_rewind()
 	test_recover_seed_from_state()
 	test_recover_seed()
 	test_recover_byteseed()
